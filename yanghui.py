0b6c92d07c10976345b2b19b3ce9da210448f2a3
becaning <973313003@qq.com>
2014-04-14 15:51:35 +0800

    change yanghui.py

--- yanghui.py
+++ yanghui.py
@@ -5,17 +5,21 @@
 '''
 total = int(raw_input('input a number: '))
 
-l = [1,[1,1]]
-for i in range(1,total):
-    num = len(l[i])
+if( total > 2):
+    print '1' 
+l = [1]
+for i in range(0,total):
+    num = len(l)
     temp=[]
     for j in range(1,num):
-        temp.append(l[i][j-1] + l[i][j]) 
+        temp.append(l[j-1] + l[j]) 
 
     temp.append(1)
-    temp.reverse()
-    temp.append(1)
-    l.append(temp)
-
-for i in range(total):
-    print l[i]
+    temp.insert(0,1)
+    l = temp
+    temp = map(str, temp)
+    s = ' '.join(temp)
+    lon = len(s)/2
+#    for j in range(total - lon):
+#        print '  ',
+    print s 
